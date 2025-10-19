dc263e3c928cb7c08578acd0e741314d7ca4c80a
lebrancconvas <lebrancconvas@gmail.com>
2025-10-19 16:49:20 +0700

    ✨ feat(backend): create api for downloading .mp4 video from url.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,28 @@
-import { Elysia } from "elysia";
+import { Elysia, t } from "elysia";
+import { cors } from "@elysiajs/cors";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(8001);
+const app = new Elysia()
+  .use(cors())
+  .get("/", () => "Hello Elysia")
+  .post("/download", ({ body, set }) => {
+    // const getTitleScript = ["yt-dlp", "get-title", body.videoURL];
+    const downloadScript = ["yt-dlp", body.videoURL, "-t", "mp4"];
+
+    const downloadProc = Bun.spawn(downloadScript);
+
+    set.headers["Content-Type"] = "video/mp4";
+    set.headers["Content-Disposition"] = "attachment; filename=video.mp4";
+
+    return {
+      message: "Download Success",
+      data: downloadProc.stdout
+    };
+  }, {
+    body: t.Object({
+      videoURL: t.String({ format: "uri" })
+    })
+  })
+  .listen(8001);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
